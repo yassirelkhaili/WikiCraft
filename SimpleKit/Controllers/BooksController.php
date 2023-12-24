c34c3b58f9cc73f015a0e38097aa06f7ad818b08
yassirelkhaili <elkhailiyassir@gmail.com>
2023-12-24 09:56:20 +0100

    finished crud example

--- SimpleKit/Controllers/BooksController.php
+++ SimpleKit/Controllers/BooksController.php
@@ -3,8 +3,8 @@
 namespace SimpleKit\Controllers;
 
 use function SimpleKit\Helpers\redirect;
-use SimpleKit\Helpers\Request;
 use SimpleKit\Models\Books;
+use SimpleKit\Helpers\Request;
 
 require __DIR__ . "/../Helpers/Redirector.php";
 
@@ -25,6 +25,11 @@ class BooksController extends BaseController {
         $this->render('Books/index', ['books' => $books]);
     }
 
+    public function create() {
+        // Render the view for creating a new book
+        $this->render('book/create');
+    }
+
     public function store(Request $request) {
         $data = $request->getPostData();
         // Create a new book using the books
@@ -40,26 +45,36 @@ class BooksController extends BaseController {
         $book = $this->books->getById($id);
 
         // Render the view and pass the book data to it
+        // $this->render('book/show', ['book]' => $book]);
         http_response_code(200);
         echo json_encode($book);
+    }
+
+    public function edit(int $id) {
+        // Fetch a specific book by ID using the books
+        $book = $this->books->getById($id);
+
+        // Render the view for editing the book
+        $this->render('book/edit', ['book' => $book]);
     }
 
     public function update(Request $request, int $id) {
         // Validate the request data (assuming a simple validation here)
         $data = $request->getPostData();
-        // Update the book using the BooksModal
+        
+        // Update the book using the booksModal
         $this->books->updateById($id, $data);
 
         // Redirect back to the index page with a success message (or handle differently based on your needs)
-        return redirect('/books')->with(['modified' => 'Book updated successfully!']);
+        return redirect('/books')->with(['modified' => 'book has been updated!']);
     }
 
-    public function destroy(int $id) {
+    public function destroy($id) {
         // Delete a specific book by ID using the books
         $this->books->deleteById($id);
 
         // Redirect back to the index page with a success message (or handle differently based on your needs)
-        return redirect('/books')->with(['success', 'book updated successfully!']);
+        return redirect('/books')->with(['destroy' => 'book was deleted']);
     }
 
     // You can add more controller methods as needed to handle other book-related functionalities
