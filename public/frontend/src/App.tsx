5418346ed06e14f5be201decc2252eb6aba1ca26
yassirelkhaili <elkhailiyassir@gmail.com>
2024-01-03 22:21:34 +0100

    finished intergrating the SimpleKit with CRA

---
+++ public/frontend/src/App.tsx
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './styles/App.css';
+import logo from "./images/logo.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello ReactJs
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
