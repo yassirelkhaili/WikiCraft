7c84006fd844a196da7cdd4e58d265643e8b4cac
yassirelkhaili <elkhailiyassir@gmail.com>
2024-01-07 01:40:26 +0100

    initial commit

--- SimpleKit/Views/index.php
+++ SimpleKit/Views/index.php
@@ -1,4 +1,4 @@
 <?php include(__DIR__ . "./layouts/header.php"); ?>
 <h1>HomePage</h1>
-<div id="root"></div> //React Application Root Element/DOM node
+<div id="root"></div> <!-- React Application Root Element/DOM Node -->
 <?php include(__DIR__ . "./layouts/footer.php"); ?>
