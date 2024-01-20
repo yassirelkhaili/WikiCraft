39c802b20cc4e6ee2ef2737b2898236c84f8e69d
yassirelkhaili <elkhailiyassir@gmail.com>
2024-01-20 23:14:01 +0100

    fixed media import dynamically for SimpleKit framework from react static file media directory

--- SimpleKit/Views/layouts/header.php
+++ SimpleKit/Views/layouts/header.php
@@ -4,27 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?= $cssurl ?>">
-    <script src="https://cdn.tailwindcss.com"></script>
-<script>
-    tailwind.config = {
-      theme: {
-        extend: {
-          colors: {
-            primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"},
-            main_header: '#1F2937',
-            border_color: '#374151',
-            main_background_color: '#111827'
-          }
-        }
-      }
-    }
-  </script>
     <title><?= $pageTitle ?></title>
 </head>
 <body class="bg-main_background_color">
 <header class="absolute w-full flex px-6 justify-between items-center h-20 border-b-[1px] border-border_color bg-main_header">
 <div>
-    <img src="../../public/frontend/src/images/brandlogo.webp" alt="WebCraft logo" class="h-8 w-56">
+    <img src="<?= $media['brandlogo'] ?>" alt="WebCraft logo" class="h-8 w-56">
 </div>
 <div class="flex justify-center items-center gap-3">
 <?php if (isset($_SESSION["session_token"])): ?>
